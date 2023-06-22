652e102c83610a54b1eb11244a2e23a44033ee5b
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-06-22 16:08:29 +0100

    updatenotworking

--- script.js
+++ script.js
@@ -15,35 +15,52 @@ function Location(storeName, minCust, maxCust, avgCookiesPerHour){
     this.maxCust = maxCust;
     this.avgCookiesPerHour = avgCookiesPerHour;
     this.custPerHour = [];
-    this.CookiesPerHour = []; 
+    this.cookiesPerHour = []; 
     this.totalCookies = 0;
    // this.calculateCookiesPerHour();
 }
 
 Location.prototype.calculateCustPerHour = function () {
-for (let i = 0; i < hours.length; i++ ) {
+for (let i = 0; i < hours.length; i++) {
     this.custPerHour.push(randomNum(this.minCust, this.maxCust));
     }
     console.log(this.custPerHour)
 
 };
 
-Location.prototype.CookiesPerHour = function () {
-this.calculateCustPerHour()
-for (let i = 0; i < hours.length; i++) {
-const cookies = Math.ceil(this.custPerHour[i] * this.avgCookiesPerHour);    
-this.CookiesPerHour.push(cookies);
-this.totalCookies += cookies;
+Location.prototype.cookiesPerHour = function () {
+    for (let i = 0; i < hours.length; i++) {
+    const cookies = Math.ceil(this.custPerHour[i] * this.avgCookiesPerHour);    
+    this.cookiesPerHour.push(cookies);
+    //this.totalCookies += cookies;
 }
-console.log(this.CookiesPerHour)
+console.log(this.CookiesPerHour);
 };
 
 Location.prototype.render = function () {
     console.log("render working")
+    this.calculateCustPerHour();
+    this.cookiesPerHour();
+
+const table = document.getElementById("myTable");
+
+const tr = document.getElementById("tr");
+table.appendChild(tr);
+
+let td = document.createElement("td");
+td.textContent = this.name
+tr.appendChild(td)
+
+total = total + this.cookiesPerHour[i]
+
+
+
 }
 
 
 const seattle = new Location("seattle", 23, 65, 6.3)
+
+seattle.render();
 
 console.log(seattle)
 
